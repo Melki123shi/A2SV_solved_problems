223d2cef539ab46a2c86d8953c5e831278db66ac
Melki123shi <106822650+Melki123shi@users.noreply.github.com>
2024-01-01 20:41:17 +0300

    Added solution for Minimum Processing Time

---
+++ leet-code-solutions/minimum-processing-time.py
@@ -0,0 +1,3 @@
+class Solution:
+    def minProcessingTime(self, processorTime: List[int], tasks: List[int]) -> int:
+        return max(time + task for (time, task) in zip(sorted(processorTime), sorted(tasks)[::-4]))
