500f84c37a33b3ecb933d55ad34e3eb692af7a87
Melki123shi <106822650+Melki123shi@users.noreply.github.com>
2024-02-08 16:12:02 +0300

    Add solution for Shifting Letters II

---
+++ leetcode/leetcode/shifting-letters-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        prefix_sum = [0] * (len(s) + 1)
+        shifted_str = ''
+
+        # use prefix sum to store the current update of a character in the string
+        for start, end, step in shifts:
+            if step == 0:
+               prefix_sum[start] -= 1
+               prefix_sum[end + 1] += 1
+            else:
+                prefix_sum[start] += 1
+                prefix_sum[end + 1] -= 1
+
+        total = 0
+        for ind in range(len(prefix_sum)):
+            total += prefix_sum[ind]
+            prefix_sum[ind] = total
+            
+        # Iterate through the prefix_sum to update the characters in the string
+        for i in range(len(prefix_sum) - 1):
+            prefix_sum[i] %= 26
+            if prefix_sum[i] < 0:
+                prefix_sum[i] = 26 + prefix_sum[i]
+            shifted_str += chr(((ord(s[i]) - 97 + prefix_sum[i]) % 26) + 97)
+        
+        return shifted_str
+
+        
