20e9c6a5b5d50424ed7730a3c8215ed9636ae725
Melki123shi <106822650+Melki123shi@users.noreply.github.com>
2024-02-11 05:17:50 +0300

    Add solution for Corporate Flight Bookings

--- leetcode/leetcode/corporate-flight-bookings.py
+++ leetcode/leetcode/corporate-flight-bookings.py
@@ -1,14 +1,15 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        res = [0] * (n+1)
-        for idx in range(len(bookings)):
-            start, end = bookings[idx][0], bookings[idx][1]
-            res[start] += bookings[idx][2]
-            if end < n:
-                res[end+1] += -bookings[idx][2]
+        prefix_sum = [0] * (n + 1) #preserve n + 1 seats to include the edge case
+
+        for start, end, seats in bookings:
+            prefix_sum[start - 1] += seats
+            prefix_sum[end] -= seats
+
+        answer = [prefix_sum[0]]
+        for i in range(len(prefix_sum) - 2):
+            answer.append(answer[i] + prefix_sum[i + 1])
         
-        for idx in range(1,n+1):
-            res[idx] += res[idx-1]
-        res.pop(0)
-        return res
-    
+        return answer
+
+
