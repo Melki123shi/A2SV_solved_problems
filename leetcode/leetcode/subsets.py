3c28570689a9c807effd1ce0896c7b8b8c143223
Melki123shi <106822650+Melki123shi@users.noreply.github.com>
2024-02-27 16:54:31 +0300

    Add solution for Subsets

--- leetcode/leetcode/subsets.py
+++ leetcode/leetcode/subsets.py
@@ -2,7 +2,7 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
         path = []
-        s_set = set()
+        s_set = Counter(nums)
 
         def backtracking(current_index):
             value = path.copy()
@@ -10,17 +10,17 @@ class Solution:
             if value not in result:
                 result.append(value)
 
-            s_set = set(path)
-            
             for index in range(current_index + 1, len(nums)):
                 candidate = nums[index]
-                if candidate not in s_set:
+                if s_set[candidate] > 0:
                     path.append(nums[index])
+                    s_set[candidate] -= 1
 
                     backtracking(current_index + 1)
+                    s_set[candidate] += 1
 
                     path.pop()
-        
+    
         backtracking(-1)
         result = sorted(result, key=lambda x : x[0] if x else 0)
         return result
